3bb4fc9e87ff5ad10ccbe4c203db717e69f513d1
vkarasas <vkarasas@gmail.com>
2025-11-16 03:29:53 +0200

    extract to JSON file messages from text channels

--- src/config/MessageFetcher.java
+++ src/config/MessageFetcher.java
@@ -1,13 +1,15 @@
 package config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class MessageFetcher {
 
@@ -17,13 +19,46 @@ public class MessageFetcher {
      * @param guildChannels List<GuildChannel>
      */
     public static void listenOldMessages(JDA jda, List<GuildChannel> guildChannels) {
+
+        List<Map<String, Object>> channelListJson = new ArrayList<>();
         for(GuildChannel guildChannel : guildChannels) {
             TextChannel textChannel = jda.getTextChannelById(guildChannel.getId());
             if (textChannel != null) {
                 List<Message> messages = loadAllMessages(textChannel);
                 messages.forEach(message -> System.out.println(guildChannel.getName() + " " + message.getContentRaw()));
+                channelListJson.add(mapChannelJson(messages, textChannel));
             }
         }
+
+        extractToJson(channelListJson);
+    }
+
+    private static void extractToJson(List<Map<String, Object>> channelListJson) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter("discord_dump.json")){
+            gson.toJson(channelListJson, writer);
+            System.out.println("Saved all messages to discord_dump.json JSON:" +   channelListJson);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static  Map<String, Object> mapChannelJson(List<Message> messages, TextChannel textChannel) {
+        List<Map<String, Object>> msgJsonList = new ArrayList<>();
+
+        for(Message message : messages) {
+            Map<String, Object> msgJson = new LinkedHashMap<>();
+            msgJson.put("id", message.getId());
+            msgJson.put("author", message.getAuthor().getName());
+            msgJson.put("content", message.getContentRaw());
+            msgJsonList.add(msgJson);
+        }
+
+        Map<String, Object> channelJson = new LinkedHashMap<>();
+        channelJson.put("id", textChannel.getId());
+        channelJson.put("name", textChannel.getName());
+        channelJson.put("messages", msgJsonList);
+        return channelJson;
     }
 
     private static List<Message> loadAllMessages(TextChannel channel) {
